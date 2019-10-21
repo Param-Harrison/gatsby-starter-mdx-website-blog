aece62b52e36fc049ac4cab3b6007b92a2efbe47
Doak Heggeness <doak@doakheggeness.net>
2019-10-21 10:22:12 -0700

    prismjs

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
             resolve: `gatsby-remark-smartypants`,
           },
           {
-            resolve: `gatsby-plugin-prismjs`,
+            resolve: `gatsby-remark-prismjs`,
           },
         ],
         plugins: [ `gatsby-remark-images`],

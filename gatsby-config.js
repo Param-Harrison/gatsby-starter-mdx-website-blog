d8737dbeda94736c4205d1aebcca3cc137f87329
Doak Heggeness <doak@doakheggeness.net>
2019-10-28 11:23:09 -0700

    mdx source

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,13 +31,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: `pages`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: ['.mdx', '.md'],
